a69582f580e17ae02d582193003b5b8ac94ebe3f
tarun <tarusuresh351996@gmail.com>
2018-05-15 12:00:34 +0530

    Add files via upload

---
+++ main/java/com/example/tarun/androidchatapp/UserDetails.java
@@ -0,0 +1,12 @@
+package com.example.tarun.androidchatapp;
+
+/**
+ * Created by tarun on 05-04-2018.
+ */
+
+    public class UserDetails {
+        static String username = "";
+        static String password = "";
+        static String chatWith = "";
+    }
+
